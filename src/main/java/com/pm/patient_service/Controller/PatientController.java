df8afa5ea494904008d789c19ad9f8d845fbbe18
Admin <somularamalingeswarreddy2@gmail.com>
2025-07-16 19:13:26 +0530

    Added the update na d delete rndpoints

--- src/main/java/com/pm/patient_service/Controller/PatientController.java
+++ src/main/java/com/pm/patient_service/Controller/PatientController.java
@@ -4,6 +4,8 @@ import com.pm.patient_service.dto.PatientRequestDto;
 import com.pm.patient_service.dto.PatientResponseDto;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,26 +13,43 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/patient")
+@Tag(name="Patient", description="API used for managing the Patients")
 public class PatientController {
 
     @Autowired
     private PatientService patientService;
 
     @GetMapping("/getPatient")
+    @Operation(summary = "Get Patients")
     private ResponseEntity<List<PatientResponseDto>> getPatients(){
         List<PatientResponseDto> patientDto = patientService.getPatients();
         return ResponseEntity.ok().body(patientDto);
     }
 
     @PostMapping("/createPatient")
+    @Operation(summary = "Create the new patient")
     private ResponseEntity<PatientResponseDto> createPatient(@Valid @RequestBody PatientRequestDto patientRequestDto){
         PatientResponseDto patientResponseDto = patientService.createPatient(patientRequestDto);
         return ResponseEntity.ok().body(patientResponseDto);
     }
 
+    @PutMapping("/updatePatient/{id}")
+    @Operation(summary = "Updating the Patient details")
+    private ResponseEntity<PatientResponseDto> updatePatient(@PathVariable UUID id, @RequestBody PatientRequestDto patientRequestDto){
+        PatientResponseDto patientResponseDto = patientService.updatePatient(id, patientRequestDto);
+        return ResponseEntity.ok().body(patientResponseDto);
+    }
+
+    @DeleteMapping("/deletePatient/{id}")
+    @Operation(summary = "Deleting the Patient")
+    private ResponseEntity<String> deletePatient(@PathVariable UUID id){
+        patientService.deletePatient(id);
+        return ResponseEntity.ok().body("Deleted Successfully");
+    }
 
 }
--- src/main/java/com/pm/patient_service/service/PatientService.java
+++ src/main/java/com/pm/patient_service/service/PatientService.java
@@ -2,6 +2,8 @@ package com.pm.patient_service.service;
 
 import com.pm.patient_service.dto.PatientRequestDto;
 import com.pm.patient_service.dto.PatientResponseDto;
+import com.pm.patient_service.exception.EmailAlreadyExistException;
+import com.pm.patient_service.exception.PatientNotFoundException;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
@@ -9,7 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -30,9 +34,28 @@ public class PatientService {
     }
 
     public PatientResponseDto createPatient(PatientRequestDto patientRequestDto){
+        if(patientRepository.existsByEmail(patientRequestDto.getEmail())){
+            throw new EmailAlreadyExistException("A patient with this email " + " already exists" + patientRequestDto.getEmail());
+        }
         Patient patient = patientRepository.save(PatientMapper.toModel(patientRequestDto));
         return PatientMapper.toDto(patient);
     }
 
+    public PatientResponseDto updatePatient(UUID id,PatientRequestDto patientRequestDto){
+        Patient patient = patientRepository.findById(id).orElseThrow(() -> new PatientNotFoundException("Patient not found by id: " + id));
+        if(patientRepository.existsByEmail(patientRequestDto.getEmail())) {
+            throw new EmailAlreadyExistException("A patient with this email " + " already exists" + patientRequestDto.getEmail());
+        }patient.setName(patientRequestDto.getName());
+        patient.setEmail(patientRequestDto.getEmail());
+        patient.setAddress(patientRequestDto.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDto.getDateOfBirth()));
 
+        Patient updatePatient = patientRepository.save(patient);
+        return PatientMapper.toDto(updatePatient);
+
+    }
+
+    public void deletePatient(UUID id) {
+        patientRepository.deleteById(id);
+    }
 }
